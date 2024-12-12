5e72f218b1613a5bdeaa76036fa6b615264dde50
João Pedro Andrade dos Santos <santos.joao@unifesp.br>
2024-12-11 21:08:47 -0300

    Update InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> //clock(), CLOCKS_PER_SEC e clock_t
+#include <time.h> 
 
-const int TAM = 10;
+int const TAM = 400000; 
 
-void Insercao(int n, int v[]) {
-    int i, j, x;
-    for (j = 1; /*A*/ j < n; j++) {
-        x = v[j];
-        for (i = j-1; i >= 0 && v[i] > x; i--){
-            v[i+1] = v[i];
-            v[i+1] = x;
-        }
-    }
+void Insercao (int n, int v[]) {
+	int i, j, x;
+	for (j = 1; /*A*/ j < n; j++) {
+		x = v[j];
+		for (i = j-1; i >= 0 && v[i] > x; i--)
+			v[i+1] = v[i];
+		v[i+1] = x;
+	}
 }
 
 int main(){
-    clock_t t; //variÃ¡vel para armazenar tempo
-    int vetor[10];
+    clock_t t;  //variÃ¡vel para armazenar tempo
+    int vetor[TAM];
     int a;
     
     //semente de aleatoriedade
@@ -33,7 +32,7 @@ int main(){
     t = clock() - t; //tempo final - tempo 
 
     //imprime o tempo na tela
-    printf("Tempo de execucao: %lf ms\n", ((double)t)/((CLOCKS_PER_SEC/1000))); //conversÃ£o para double
+    printf("Tempo de execucao: %lf ms\n", ((double)t)/((CLOCKS_PER_SEC/1000)));
 
     return 0;
 }
