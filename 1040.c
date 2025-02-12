867af08d1b2585e49fa2760a9464fabfbbe7f6a9
João Pedro Andrade dos Santos <santos.joao@unifesp.br>
2025-02-12 18:07:13 -0300

    Create 1040.c

---
+++ 1040.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    double n1, n2, n3, n4, exame, media, mediaf;
+    scanf("%lf %lf %lf %lf", &n1, &n2, &n3, &n4);
+    media = (n1 * 2 + n2 * 3 + n3 * 4 + n4 * 1) / 10;
+    printf("Media: %.1lf\n", media);
+    
+    if (media >= 7.0) {
+        printf("Aluno aprovado.\n");
+    } else if (media < 5.0) {
+        printf("Aluno reprovado.\n");
+    } else {
+        printf("Aluno em exame.\n");
+        scanf("%lf", &exame);
+        printf("Nota do exame: %.1lf\n", exame);
+        mediaf = (media + exame) / 2;
+        
+        if (mediaf >= 5.0) {
+            printf("Aluno aprovado.\n");
+        } else {
+            printf("Aluno reprovado.\n");
+        }
+        printf("Media final: %.1lf\n", mediaf);
+    }
+    return 0;
+}
