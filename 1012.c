0482f5d9487377500a3d15a9d8276d2dfbf1ee2e
João Pedro Andrade dos Santos <santos.joao@unifesp.br>
2025-02-12 18:50:49 -0300

    Create 1012.c

---
+++ 1012.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main() {
+    double a; double b; double c;
+    double resultado;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    
+    resultado = a*c/2;
+    printf("TRIANGULO: %.3lf\n", resultado);
+    
+    resultado = 3.14159*c*c;
+    printf("CIRCULO: %.3lf\n", resultado);
+    
+    resultado = (a+b)*c/2;
+    printf("TRAPEZIO: %.3lf\n", resultado);
+    
+    resultado = b*b;
+    printf("QUADRaDO: %.3lf\n", resultado);
+
+    resultado = a*b;
+    printf("RETANGULO: %.3lf\n", resultado);
+    return 0;
+}
